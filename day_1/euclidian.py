f6c13dd0b674b786743e041f95726ae7baacb976
bagwai <mubarakdaha8@gmail.com>
2023-01-24 07:24:22 +0100

    submitting day_1 exercises

---
+++ day_1/euclidian.py
@@ -0,0 +1,15 @@
+#Find an Euclidian distance between (2, 3) and (10, 8)
+#The Euclidean distance between two points in Euclidean space is the length of a line segment between the two points.
+
+p1, q1 = 2, 3
+p2, q2 = 10, 8
+
+e_distance = pow((pow((q2 - q1), 2) + pow((p2 - p1), 2)), 0.5)
+
+print(e_distance)  #9.433981132056603
+
+
+
+
+
+
---
+++ day_1/helloworld.py
@@ -0,0 +1,77 @@
+# Day 1 - 30DaysOfPython Challenge
+# Mubarak Daha Isa - ADS '23 Fellow
+# Exercise Level 1
+
+# 1. Mathmatical operations. The operands are 3 and 4
+
+print(4 + 3)          #7
+print(4 - 2)          #2 
+print(4 * 3)          #12
+print(4 / 3)          #1.3333333  
+print(4 ** 3)         #64  
+print(4 % 3)          #1   
+print(4 // 3)         #1   
+
+#2. Strings
+
+print("Mubarak")      #Mubarak  
+print("Daha Isa")     #Daha Isa  
+print("Nigeria")      #Nigeria  
+print("I am enjoying 30 days of python") #I am enjoying 30 days of python
+
+#3. Checking data types
+print(type(10))          #<class 'int'>
+print(type(9.8))         #<class 'float'>
+print(type(3.14))        #<class 'float'>
+print(type(4 - 4j))      #<class 'complex'>
+print(type(['Asabeneh', 'Python', 'Finland']))  #<class 'list'>
+print(type("Mubarak"))  #String                 #<class 'str'>
+print(type("Daha Isa")) # String                #<class 'str'>
+print(type("Nigeria"))    # String              #<class 'str'>
+
+#Exercise: Level 3
+#1. example for different Python data types
+
+#Number
+#Integer: Integer(negative, zero and positive) numbers 
+# Example:  -300, -200, -100, 0, 100, 200, 300 ...
+#Float: Decimal number 
+# Example ... -3.5, -2.25, -1.0, 0.0, 1.1, 2.2, 3.5 ...
+#Complex Example 1 + j, 2 + 4j, 2 + 7j
+
+
+#String
+#Example: 'Nigeria', 'North' 'Kano' 'Arewa Data Science'
+#'Enjoying my first Data Science Fellowship'
+
+#Booleans
+#Example:
+#True  #  Is the checkbox checked? If it is checked, then the value is True
+#False # Is the checkbox checked? If it is not checked, then the value is False
+
+#List
+#Example:
+#[50, 45, 34, 60, 40, 90]  # all are the same data types - a list of student scores
+#['Babangida', 'Mahmoud', 'Auwal', 'Sani'] # all the same data types - a list of strings names
+#['Banana', 10, False, 9.81] # different data types in the list - string, integer, boolean and float
+
+#Dictionary
+#Example:
+# {
+# 'first_name':'Mubarak',
+# 'last_name':'Daha Isa',
+# 'country':'Nigeria', 
+# 'age':30, 
+# 'is_married':False,
+# 'skills':['JS', 'React', 'Cloud', 'Python']
+# }
+
+
+#Tuple
+#Example:
+#('Babangida', 'Mahmud', 'Auwal', 'Sani', 'Mubarak') # Names
+
+#Set
+#Example:
+# {"CST/16/COM/001","CST/16/COM/002", "CST/16/COM/001"} #Unique set of students IDs
+
